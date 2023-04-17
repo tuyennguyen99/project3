8ee798a5e1352fb4b3f06384b2adbc3fce45ee62
tuyennguyen99 <nguyentuyen95.bt@gmail.com>
2023-04-17 22:38:05 +0700

    Remove todo

--- web/app/routes.py
+++ web/app/routes.py
@@ -71,16 +71,6 @@ def notification():
             ## TODO: Refactor This logic into an Azure Function
             ## Code below will be replaced by a message queue
             #################################################
-            # attendees = Attendee.query.all()
-
-            # for attendee in attendees:
-            #     subject = '{}: {}'.format(attendee.first_name, notification.subject)
-            #     send_email(attendee.email, subject, notification.message)
-
-            # notification.completed_date = datetime.utcnow()
-            # notification.status = 'Notified {} attendees'.format(len(attendees))
-            # db.session.commit()
-            # TODO Call servicebus queue_client to enqueue notification ID
             sender = queue_client.get_sender()
             sender.send(Message(str(notification.id)))
 
